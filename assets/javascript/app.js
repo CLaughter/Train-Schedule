f87e943f29cf3f80f3e94dc2e6dc1c26ed1f2851
Chris Laughter <chrislaugh@gmail.com>
2019-10-01 16:36:28 -0600

    work in progress

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,28 @@
 var firebaseConfig = {
-  apiKey: "AIzaSyBrG3NoJyDT_1BG1OqwDbdNyP375JEASkk",
-  authDomain: "clickcountdown-66c39.firebaseapp.com",
-  databaseURL: "https://clickcountdown-66c39.firebaseio.com",
-  projectId: "clickcountdown-66c39",
-  storageBucket: "gs://clickcountdown-66c39.appspot.com",
-  messagingSenderId: "845019678196",
-  appId: "1:845019678196:web:b31c95c415037790d925a1",
-  measurementId: "G-0TQB95TTXY"
+    apiKey: "AIzaSyBUdDf6AS6nFHuH8qUn_dSw9Go-Lg9PWYA",
+    authDomain: "train-schedule-97b8b.firebaseapp.com",
+    databaseURL: "https://train-schedule-97b8b.firebaseio.com",
+    projectId: "train-schedule-97b8b",
+    storageBucket: "",
+    messagingSenderId: "975374873087",
+    appId: "1:975374873087:web:7d6b3f0cf44de99ab5b7ca",
+    measurementId: "G-KLQGTT2RNB"
 };
 
 // Initialize
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
 
-$(document).ready(function() {
+// $(document).ready(function() {
   console.log("Is this thing working?");
-  // var makeRow = function() {
-  // $('#newRow').empty(); 
-  //   $(newRow.appendTo('row'));
-  // }
-  // makeRow();
 
+  // Set current time under main title
   setInterval(function(){
     var now = moment().format('LLLL');
     $('#timeDate').text(now);
    }, 30000);
 
+  //  Grabs user input
   $("#addTrain").on('click', function(event) {
     event.preventDefault();
     var trainName = $('#trainName-input').val().trim();
@@ -33,9 +30,59 @@ $(document).ready(function() {
     var firstTime = $('firstTime-input').val().trim();
     var frequency = $('#frequency-input').val().trim();
 
-    database.ref('/data').push(data);
-  });
+  // Creates local "temporary" object for holding employee data
+  var newTrain = {
+    name: trainName,
+    location: destination,
+    time: firstTime,
+    interval: frequency
+  };
 
-  
+  // Uploads train data to database
+  database.ref('/data').push(newTrain);
+
+  // Logs everything to console
+  console.log(newTrain.name);
+  console.log(newTrain.location);
+  console.log(newTrain.time);
+  console.log(newTrain.interval);
+
+  alert("New Train added");
+
+    // Clears text-boxes
+  $("#trainName-input").val("");
+  $("#destination-input").val("");
+  $("#firstTime-input").val("");
+  $("#frequency-input").val("");
 
 });
+
+  /* Create Firebase event for adding train to database and a row in the html 
+  when a user adds an entry */
+
+  database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+  
+    // Store everything into a variable
+    var trainName = childSnapshot.val().name;
+    var destination = childSnapshot.val().location;
+    var firstTime = childSnapshot.val().time;
+    var frequency = childSnapshot.val().interval;
+  
+    // Train Info
+    console.log(trainName);
+    console.log(destination);
+    console.log(firstTime);
+    console.log(frequency);
+  
+    // Create the new row
+    var newRow = $("<tr>").append(
+      $("<td>").text(trainName),
+      $("<td>").text(destination),
+      $("<td>").text(firstTime),
+      $("<td>").text(frequency)
+    );
+  
+    // Append the new row to the table
+    $("#trainInfo-table > tbody").append(newRow);
+  });
